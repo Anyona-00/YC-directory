b67fd12f3bdd8b91299373e73952c4b9014c1b79
Anyona-00 <ombasaanyona@gmail.com>
2025-04-07 05:05:24 +0300

    reorganised the code

---
+++ .next/static/chunks/app_(root)_page_tsx_536fa247._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_(root)_page_tsx_536fa247._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_(root)_page_tsx_536fa247._.js",
+  "chunks": [
+    "static/chunks/_5089f40d._.js"
+  ],
+  "source": "dynamic"
+});
